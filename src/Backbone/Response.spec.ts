3f946db9fb68b11c014b60bdb16c84c7ec949b10
Christoph Jesaja Heidelbach <christoph_jesaja.heidelbach@tu-dresden.de>
2020-04-21 15:32:46 +0200

    add first basic test via mocha + chai

---
+++ src/Backbone/Response.spec.ts
@@ -0,0 +1,14 @@
+import { SuccessResponse, ErrorResponse } from './Response';
+import { expect } from 'chai';
+import 'mocha';
+
+describe('SuccessResponse', () => {
+    it('method: initialize()', () => {
+        let response = new SuccessResponse();
+        expect(response.getMessage()).to.equal('');
+        expect(JSON.stringify(response.getContent())).to.equal(JSON.stringify({}));
+        const msg = 'This is a test message!';
+        const obj = {hello: 'test'}
+        expect(response.initialize(msg, obj)).is.true
+    });
+});
--- src/Backbone/Response.ts
+++ src/Backbone/Response.ts
@@ -1,15 +1,24 @@
 export interface IResponse {
     getMessage(): string
     getContent(): object
+    initialize(message: string, content: object): boolean
 }
 
 abstract class Response implements IResponse {
-    protected message: string = "";
+    protected message: string = '';
     protected content: object = {};
+    protected initialized: boolean = false;
 
-    constructor(message: string, content: object) {
-        this.message = message;
-        this.content = content;
+    constructor() {}
+
+    initialize(message: string, content: object): boolean {
+        if (!this.initialized) {
+            this.message = message;
+            this.content = content;
+            return (this.message == message && JSON.stringify(this.content) == JSON.stringify(content));
+        } else {
+            return false
+        }
     }
 
     getMessage(): string {
@@ -21,11 +30,11 @@ abstract class Response implements IResponse {
     }
 }
 
-class SuccessResponse extends  Response {
+export class SuccessResponse extends Response {
 
 }
 
-class ErrorResponse extends  Response {
+export class ErrorResponse extends Response {
 
 }
 
@@ -39,12 +48,16 @@ export abstract class ResponseFactory implements IResponseFactory {
 
 export class SuccessResponseFactory extends ResponseFactory {
     create(message: string, content: object): IResponse {
-        return new SuccessResponse(message, content);
+        let response = new SuccessResponse();
+        response.initialize(message, content)
+        return response;
     }
 }
 
 export class ErrorResponseFactory extends ResponseFactory {
     create(message: string, content: object): IResponse {
-        return new ErrorResponse(message, content);
+        let response = new ErrorResponse();
+        response.initialize(message, content)
+        return response;
     }
 }
