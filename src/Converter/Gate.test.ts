1ac4f534fa5b605545d2c8d76c711affb477feae
Christoph Jesaja Heidelbach <christoph_jesaja.heidelbach@tu-dresden.de>
2020-04-23 17:14:50 +0200

    fill Gate.test.ts with different testcases

--- src/Converter/Gate.test.ts
+++ src/Converter/Gate.test.ts
@@ -1,5 +1,29 @@
 import { expect } from 'chai';
+import {FMTPGate, MTPGate} from "./Gate";
 
-describe("", () => {
+describe("class MTPGate", () => {
+    const factory = new FMTPGate()
+    let gate = factory.create()
+    it("method: initialize", () => {
+        expect(gate.initialize()).is.false;
+    })
+    it("method: send", () => {
+        expect(gate.send()).is.false;
+    })
+    it("method: receive", () => {
+        expect(gate.receive()).is.false;
+    })
+    it("method: open", () => {
+        expect(gate.open()).is.false;
+    })
+    it("method: close", () => {
+        expect(gate.close()).is.false;
+    })
+})
 
+describe("class: FMTPGate", () => {
+    const factory = new FMTPGate();
+    it("method: create()", () => {
+        expect(typeof factory.create()).is.equal(typeof new MTPGate())
+    })
 });
--- src/Converter/Gate.ts
+++ src/Converter/Gate.ts
@@ -6,6 +6,34 @@ export interface IGate {
     initialize(): boolean;
 }
 
+export class MTPGate implements IGate {
+    private file: any;
+
+    send(): any {
+        return false
+    };
+    receive(): any {
+        return false
+    };
+    open(): any {
+        return false
+    };
+    close(): any {
+        return false
+    };
+    initialize(): boolean {
+        return false
+    };
+}
+
+/* Factory */
+
+export class FMTPGate implements IFGate {
+    create(): IGate {
+        return new MTPGate();
+    }
+}
+
 export interface IFGate {
     create(): IGate;
 }
