32418595e76b821d0094b0a54064b99f5e98a870
Christoph Jesaja Heidelbach <christoph_jesaja.heidelbach@tu-dresden.de>
2020-04-29 14:29:23 +0200

    Add semantic versioning model, add test: classBasicSemanticVersion

---
+++ src/Backbone/SemanticVersion.ts
@@ -0,0 +1,77 @@
+
+abstract class ASemanticVersion implements SemanticVersion {
+    protected major: number;
+    protected minor: number;
+    protected patch: number;
+    protected initialized: boolean;
+    compatibility(semver: SemanticVersion): boolean {
+        if (this.equals(semver)) {
+            return true
+        }
+        if (this.major != semver.getMajor()) {
+            return false
+        } else {
+            if (this.minor < semver.getMinor()) {
+                return false
+            } else if (this.minor == semver.getMinor()) {
+                return this.patch > semver.getPatch() || this.patch == semver.getPatch();
+            } else {
+                return true;
+            }
+        }
+    };
+    equals(semver: SemanticVersion): boolean {
+        return this.major == semver.getMajor() && this.minor == semver.getMinor() && this.patch == semver.getPatch()
+    };
+    getMajor(): number {
+        return this.major;
+    };
+    getMinor(): number {
+        return this.minor;
+    };
+    getPatch(): number {
+        return this.patch
+    };
+    initialize(major: number, minor: number, patch: number): boolean {
+        if (major < 0 || minor < 0 || patch < 0 || this.initialized) {
+            return false
+        } else {
+            this.major = major;
+            this.minor = minor;
+            this.patch = patch
+            this.initialized = true;
+            return true
+        }
+    };
+
+    constructor() {
+        this.major = -1;
+        this.minor = -1;
+        this.patch = -1;
+        this.initialized = false;
+    }
+}
+
+export class BasicSemanticVersion extends ASemanticVersion {
+
+}
+
+export class BasicSemanticVersionFactory {
+
+}
+
+export class SemanticVersionVendor {
+
+}
+
+export interface SemanticVersion {
+    compatibility(semver: SemanticVersion): boolean;
+    equals(semver: SemanticVersion): boolean;
+    getMajor(): number;
+    getMinor(): number;
+    getPatch(): number;
+    initialize(major: number, minor: number, patch: number): boolean
+}
+export interface SemanticVersionFactory {
+    create():SemanticVersion;
+}
