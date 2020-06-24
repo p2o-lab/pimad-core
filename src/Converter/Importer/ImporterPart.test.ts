a4a17e109408421800cbcd5e30b2f5a34d542d21
Christoph Jesaja Heidelbach <christoph_jesaja.heidelbach@tu-dresden.de>
2020-06-24 11:27:36 +0200

    Add ImporterPart.ts + ImporterPart.test.ts

---
+++ src/Converter/Importer/ImporterPart.test.ts
@@ -0,0 +1,55 @@
+import {expect} from 'chai';
+import {HMIPart, MTPPart, ServicePart, TextPart} from './ImporterPart';
+import {ErrorResponse} from '../../Backbone/Response';
+
+describe('class: MTPPart', () => {
+    let part = new MTPPart();
+    beforeEach(() => {
+        part = new MTPPart();
+    })
+    it('method: convertFrom()', () => {
+        part.convertFrom({},(response) => {
+            expect(response.constructor.name).is.equal(new ErrorResponse().constructor.name);
+            expect(response.getMessage()).is.equal('Not implemented yet!');
+        })
+    })
+});
+
+describe('class: HMIPart', () => {
+    let part = new HMIPart();
+    beforeEach(() => {
+        part = new HMIPart();
+    })
+    it('method: convertFrom()', () => {
+        part.convertFrom({},(response) => {
+            expect(response.constructor.name).is.equal(new ErrorResponse().constructor.name);
+            expect(response.getMessage()).is.equal('Not implemented yet!');
+        })
+    })
+});
+
+describe('class: TextPart', () => {
+    let part = new TextPart();
+    beforeEach(() => {
+        part = new TextPart();
+    })
+    it('method: convertFrom()', () => {
+        part.convertFrom({},(response) => {
+            expect(response.constructor.name).is.equal(new ErrorResponse().constructor.name);
+            expect(response.getMessage()).is.equal('Not implemented yet!');
+        })
+    })
+});
+
+describe('class: ServicePart', () => {
+    let part = new ServicePart();
+    beforeEach(() => {
+        part = new ServicePart();
+    })
+    it('method: convertFrom()', () => {
+        part.convertFrom({},(response) => {
+            expect(response.constructor.name).is.equal(new ErrorResponse().constructor.name);
+            expect(response.getMessage()).is.equal('Not implemented yet!');
+        })
+    })
+});
---
+++ src/Converter/Importer/ImporterPart.ts
@@ -0,0 +1,31 @@
+import {Response, ResponseVendor} from '../../Backbone/Response';
+
+abstract class AImporterPart implements ImporterPart {
+    protected responseVendor: ResponseVendor
+
+    convertFrom(data: object, callback: (response: Response) => void) {
+        const localResponse = this.responseVendor.buyErrorResponse()
+        localResponse.initialize('Not implemented yet!', {})
+        callback(localResponse)
+    }
+    constructor() {
+        this.responseVendor = new ResponseVendor();
+    }
+}
+
+export class HMIPart extends AImporterPart {
+
+}
+export class MTPPart extends AImporterPart {
+
+}
+export class ServicePart extends AImporterPart {
+
+}
+export class TextPart extends AImporterPart {
+
+}
+
+export interface ImporterPart {
+    convertFrom(data: object, callback: (response: Response) => void): void;
+}
