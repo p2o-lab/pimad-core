b38ac66fd9777d6019c30e0a9e088d0a66de8c76
Christoph Jesaja Heidelbach <christoph_jesaja.heidelbach@tu-dresden.de>
2020-07-03 13:44:27 +0200

    Add doc. make it nice.

--- src/Converter/Importer/ImporterPart.ts
+++ src/Converter/Importer/ImporterPart.ts
@@ -28,38 +28,54 @@ export class HMIPart extends AImporterPart {
 export class MTPPart extends AImporterPart {
     private opcuaServerCommunicationFactory: CommunicationInterfaceDataFactory;
 
-    extract(data: {CommunicationSet: object[], HMISet: object, ServiceSet: object, TextSet: object}, callback: (response: Response) => void): void {
-        const communicationSet = this.extractCommunicationSet(data.CommunicationSet);
-        super.extract(data, callback);
+    /**
+     * Parsing the relevant data of the ModuleTypePackage-object and copy that to different instances of PiMAd-core-IM.
+     * @param data The bare ModuleTypePackage-object of the MTP. Containing a CommunicationSet, HMISet, ServiceSet and TextSet.
+     * @param callback A callback function with an instance of the Response-Interface.
+     */
+    extract(data: {CommunicationSet: object[]; HMISet: object; ServiceSet: object; TextSet: object}, callback: (response: Response) => void): void {
+        const communicationSet = this.buildCommunicationSet(data.CommunicationSet);
+        // TODO: Überprüfen ob Extraktion erfolgreich war.
+        const localeResponse = this.responseVendor.buySuccessResponse();
+        localeResponse.initialize('Success!', communicationSet);
+        callback(localeResponse);
     }
 
     /**
-     * Extract the CommunicationSet
-     * @param data
+     * Initialize all relevant Data of 'MTP-CommunicationSet' as instances of PiMAd-core-IM.
+     * @param communicationSet The bare CommunicationSet-object of the MTP.
      */
-    private extractCommunicationSet(data: object[]): {
-        CommunicationInterfaceData: CommunicationInterfaceData[],
-        DataAssemblies: DataAssembly[]
+    private buildCommunicationSet(communicationSet: object[]): {
+        CommunicationInterfaceData: CommunicationInterfaceData[];
+        DataAssemblies: DataAssembly[];
     }  {
-        let communicationInterfaceData: CommunicationInterfaceData[] = [];
-        let dataAssemblies: DataAssembly[] = []
-        let externalInterfaces: {Name: string, ID: string, RefBaseClassPath: string, Attribute: {Name: string, AttributeDataType: string, Value: string}[]}[] = []
+        const communicationInterfaceData: CommunicationInterfaceData[] = [];
+        const dataAssemblies: DataAssembly[] = [];
+        //let externalInterfaces: {Name: string, ID: string, RefBaseClassPath: string, Attribute: {Name: string, AttributeDataType: string, Value: string}[]}[] = [];
 
-        data.forEach((element: {RefBaseSystemUnitPath?: string, InternalElement?: object[]}) => {
+        communicationSet.forEach((element: {RefBaseSystemUnitPath?: string; InternalElement?: object[]}) => {
             if(element.InternalElement == undefined) {
-                element.InternalElement = []
+                element.InternalElement = [];
             }
             switch (element.RefBaseSystemUnitPath) {
                 case 'MTPSUCLib/CommunicationSet/SourceList':
                     if(!(Array.isArray(element.InternalElement))) {
-                        element.InternalElement = [element.InternalElement]
+                        element.InternalElement = [element.InternalElement];
                     }
-                    element.InternalElement?.forEach((source: {Name?: string, RefBaseSystemUnitPath?: string, Attribute?: {Name: string, AttributeDataType: string, Value: string}}) => {
+                    element.InternalElement?.forEach((source: {
+                        Name?: string;
+                        RefBaseSystemUnitPath?: string;
+                        Attribute?: {
+                            Name: string;
+                            AttributeDataType: string;
+                            Value: string;
+                        };
+                    }) => {
                         switch (source.RefBaseSystemUnitPath) {
                             case 'MTPCommunicationSUCLib/ServerAssembly/OPCUAServer':
-                                const localeComIntData = this.opcuaServerCommunicationFactory.create()
+                                const localeComIntData = this.opcuaServerCommunicationFactory.create();
                                 if(localeComIntData.initialize({name: source.Name, serverURL: source.Attribute?.Value})) {
-                                    communicationInterfaceData.push(localeComIntData)
+                                    communicationInterfaceData.push(localeComIntData);
                                 }
                                 // parse the external Interface stuff
                                 break;
