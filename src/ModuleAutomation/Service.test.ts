8524f46baf3f71cdd2c2a2d193457e7ec4451571
Christoph Jesaja Heidelbach <christoph_jesaja.heidelbach@tu-dresden.de>
2020-07-24 13:38:29 +0200

    refactor initialize(), getMetaModelReference()

--- src/ModuleAutomation/Service.test.ts
+++ src/ModuleAutomation/Service.test.ts
@@ -2,13 +2,45 @@ import {BaseServiceFactory, BaseService} from './Service';
 import {expect} from 'chai';
 import {ErrorResponse} from '../Backbone/Response';
 import {BaseParameter} from './Parameter';
+import {BaseDataAssembly, DataAssembly} from './DataAssembly';
+import {Attribute} from 'AML';
+import {BaseProcedure} from './Procedure';
 
 describe('class: BaseService', () => {
     let service: BaseService;
     beforeEach(function () {
         service = new BaseService();
     });
-    it('method: getAllCommunicationInterfaceData()', () => {
+    describe('check getter', () => {
+        beforeEach(function () {
+            const dataAssembly = new BaseDataAssembly();
+            dataAssembly.initialize({
+                tag: 'Test-DataAssembly',
+                description: '',
+                metaModelRef: '',
+                identifier: '',
+                dataItems: []
+            });
+            const attributes: Attribute[] = [
+                {Name: 'Test-Attribute0', AttributeDataType: '', Value:''},
+                {Name: 'Test-Attribute1', AttributeDataType: '', Value:'1'},
+                {Name: 'Test-Attribute2', AttributeDataType: '', Value:''}
+            ];
+            const parameter = new BaseParameter();
+            parameter.initialize('Test-Parameter', [], '');
+            const parameter2 = new BaseParameter();
+            parameter2.initialize('Test-Parameter2', [], '');
+            const procedure0 = new BaseProcedure();
+            procedure0.initialize({} as DataAssembly, '','', 'Test-Procedure0', [],[]);
+            const procedure1 = new BaseProcedure();
+            procedure1.initialize({} as DataAssembly, '','', 'Test-Procedure1', [],[]);
+            service.initialize(attributes, dataAssembly,'Test-Identifier','Test-MetaModelRef','Test-Service', [parameter, parameter2], [procedure0, procedure1]);
+        });
+        it('method: getMetaModelRef()', () => {
+            expect(JSON.stringify(service.getMetaModelReference().getContent())).is.equal(JSON.stringify({data: 'Test-MetaModelRef'}));
+        });
+    })
+    /*it('method: getAllCommunicationInterfaceData()', () => {
         expect(typeof service.getAllCommunicationInterfaceData()).is.equal(typeof new ErrorResponse());
     });
     it('method: getAllProcedures()', () => {
@@ -28,9 +60,10 @@ describe('class: BaseService', () => {
     });
     it('method: getName()', () => {
         expect(typeof service.getName()).is.equal(typeof '');
-    });
+    }); */
     it('method: initialize()', () => {
-        expect(service.initialize([],'',[],[])).is.true;
+        expect(service.initialize([], {} as DataAssembly, '', '', '', [], [])).is.true;
+        expect(service.initialize([], {} as DataAssembly, '', '', '', [], [])).is.false;
     });
 });
 describe('class: BaseServiceFactory', () => {
--- src/ModuleAutomation/Service.ts
+++ src/ModuleAutomation/Service.ts
@@ -1,36 +1,61 @@
 import {Parameter} from './Parameter';
 import {Response, ResponseVendor} from '../Backbone/Response';
 import {logger} from '../Utils/Logger';
+import {DataAssembly} from './DataAssembly';
+import {Procedure} from './Procedure';
+import { Attribute } from 'AML';
 
 export interface Service {
-    getAllCommunicationInterfaceData(): Response; //CommunicationInterfaceData[];
-    getAllProcedures(): Response; //Procedure[];
-    getAllParameters(): Parameter[];
-    getCommunicationInterfaceData(tag: string): Response; //CommunicationInterfaceData;
-    getProcedure(tag: string): Response; //Procedure;
-    getParameter(tag: string): Response; //Parameter;
-    getName(): string;
-    initialize(cidata: any[], name: string, stra: any[], para: Parameter[]): boolean;
-    //initialize(cidata: CommunicationInterfaceData[], name: string, stra:Procedure[], para: Parameter[]): boolean;
+    /*getAllAttributes(): Response;
+    getAllProcedures(): Response;
+    getAllParameters(): Response;
+    getAttribute(tag: string, callback: (response: Response) => void): void;
+    getParameter(tag: string, callback: (response: Response) => void): void;
+    getProcedure(tag: string, callback: (response: Response) => void): void;
+    getDataAssembly(): Response;
+    getName(): Response;*/
+    /**
+     * Getter for this.metaModelRef of the service object.
+     * @returns A response object. {content: {data: Meta-Model-Reference}}
+     */
+    getMetaModelReference(): Response;
+    /**
+     * Initialize the service object with data. This one works like a constructor.
+     * @param attributes - An Array with attributes of the service object..
+     * @param dataAssembly - The data assembly of the service object.. F. ex. with the communication interface data.
+     * @param identifier - A general identifier of the service object.
+     * @param metaModelRef - A reference to a metamodel describing the service object.
+     * @param name - The name of the service object.
+     * @param parameter - An Array with service parameters.
+     * @param procedure - An Array with service procedures.
+     * @returns True for a successful initialisation. False for a not successful initialisation.
+     */
+    initialize(attributes: Attribute[], dataAssembly: DataAssembly, identifier: string, metaModelRef: string, name: string, parameter: Parameter[], procedure: Procedure[]): boolean;
 }
 
 abstract class AService implements Service{
-    protected ciData: any[]; //CommunicationInterfaceData[];
+    protected attributes: Attribute[];
+    protected dataAssembly: DataAssembly;
+    protected identifier: string;
+    protected metaModelRef: string;
     protected name: string;
-    protected procedures: any[]; //Procedure[];
+    protected procedures: Procedure[];
     protected parameters: Parameter[];
     protected initialized: boolean;
     protected responseVendor: ResponseVendor;
 
     constructor() {
-        this.ciData= [];
-        this.name='';
-        this.procedures=[];
+        this.attributes = [];
+        this.dataAssembly = {} as DataAssembly;
+        this.identifier = 'identifier: not initialized';
+        this.metaModelRef = 'metaModelRef: not initialized';
+        this.name = 'name: not initialized';
+        this.procedures = [];
         this.parameters = [];
-        this.initialized=false;
+        this.initialized = false;
         this.responseVendor = new ResponseVendor();
     }
-    getAllCommunicationInterfaceData(): Response{
+    /*getAllCommunicationInterfaceData(): Response{
         return this.responseVendor.buyErrorResponse();
     }
     getAllProcedures(): Response{
@@ -51,14 +76,34 @@ abstract class AService implements Service{
     }
     getName(): string {
         return this.name;
+    }*/
+    getMetaModelReference(): Response {
+        const response = this.responseVendor.buySuccessResponse();
+        response.initialize('Success!', {data: this.metaModelRef});
+        return response
     }
-    initialize(cidata: any[], name: string, stra: any[], para: Parameter[]): boolean {
-        this.ciData=cidata;
-        this.name=name;
-        this.procedures=stra;
-        this.parameters=para;
-        this.initialized=true;
-        return this.initialized;
+
+    initialize(attributes: Attribute[], dataAssembly: DataAssembly, identifier: string, metaModelRef: string, name: string, parameter: Parameter[], procedure: Procedure[]): boolean {
+        if(!this.initialized) {
+            this.attributes = attributes;
+            this.dataAssembly = dataAssembly;
+            this.identifier = identifier
+            this.metaModelRef = metaModelRef;
+            this.name = name;
+            this.parameters = parameter;
+            this.procedures = procedure;
+            this.initialized = (JSON.stringify(this.attributes) === JSON.stringify(attributes) &&
+                    JSON.stringify(this.dataAssembly) === JSON.stringify(dataAssembly) &&
+                    this.identifier === identifier &&
+                    this.metaModelRef === metaModelRef &&
+                    this.name === name &&
+                    JSON.stringify(this.parameters) === JSON.stringify(parameter) &&
+                    JSON.stringify(this.procedures) === JSON.stringify(procedure)
+            );
+            return this.initialized;
+        } else {
+            return false;
+        }
     }
 }
 export class BaseService extends AService {
