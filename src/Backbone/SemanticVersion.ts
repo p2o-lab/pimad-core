6fbfb8dfe4b8a3ce494d5764d8ca9382ad0c78c6
Christoph Jesaja Heidelbach <christoph_jesaja.heidelbach@tu-dresden.de>
2020-08-06 17:31:11 +0200

    fixing linter issues

--- src/Backbone/SemanticVersion.ts
+++ src/Backbone/SemanticVersion.ts
@@ -7,13 +7,13 @@ abstract class ASemanticVersion implements SemanticVersion {
     protected initialized: boolean;
     compatibility(semver: SemanticVersion): boolean {
         if (this.equals(semver)) {
-            return true
+            return true;
         }
         if (this.major != semver.getMajor()) {
-            return false
+            return false;
         } else {
             if (this.minor < semver.getMinor()) {
-                return false
+                return false;
             } else if (this.minor == semver.getMinor()) {
                 return this.patch > semver.getPatch() || this.patch == semver.getPatch();
             } else {
@@ -22,7 +22,7 @@ abstract class ASemanticVersion implements SemanticVersion {
         }
     };
     equals(semver: SemanticVersion): boolean {
-        return this.major == semver.getMajor() && this.minor == semver.getMinor() && this.patch == semver.getPatch()
+        return this.major == semver.getMajor() && this.minor == semver.getMinor() && this.patch == semver.getPatch();
     };
     getMajor(): number {
         return this.major;
@@ -31,17 +31,17 @@ abstract class ASemanticVersion implements SemanticVersion {
         return this.minor;
     };
     getPatch(): number {
-        return this.patch
+        return this.patch;
     };
     initialize(major: number, minor: number, patch: number): boolean {
         if (major < 0 || minor < 0 || patch < 0 || this.initialized) {
-            return false
+            return false;
         } else {
             this.major = major;
             this.minor = minor;
-            this.patch = patch
+            this.patch = patch;
             this.initialized = true;
-            return true
+            return true;
         }
     };
 
@@ -63,7 +63,7 @@ export class BasicSemanticVersion extends ASemanticVersion {
 
 export class BasicSemanticVersionFactory extends ASemanticVersionFactory{
     create(): SemanticVersion {
-        const semver = new BasicSemanticVersion()
+        const semver = new BasicSemanticVersion();
         logger.debug(this.constructor.name + ' creates a ' + semver.constructor.name);
         return semver;
     }
