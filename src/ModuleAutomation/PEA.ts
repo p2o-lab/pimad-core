6a652ab7507558611b7f8205a13b4e7fc0993e90
Tuan Son Dinh <tuansondinh96@gmail.com>
2021-05-17 16:30:19 +0200

    minor improvment

--- src/ModuleAutomation/PEA.ts
+++ src/ModuleAutomation/PEA.ts
@@ -136,7 +136,7 @@ abstract class APEA implements PEA {
     }
     getAllDataAssemblies(): PiMAdResponse {
         const response = this.responseVendor.buySuccessResponse();
-        response.initialize('Success!', {data: this.dataAssemblies});
+        response.initialize('Success!', this.dataAssemblies);
         return response;
     }
     getAllFEAs(): PiMAdResponse {
